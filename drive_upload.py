f3d23187401c3d6a7fa1b0b9d0ea9dde0ffbcc68
Raksha-2305 <cssraksha9035@gmail.com>
2026-04-29 15:09:29 +0530

    Update drive_upload.py

--- drive_upload.py
+++ drive_upload.py
@@ -1,21 +1,2 @@
-from pydrive2.auth import GoogleAuth
-from pydrive2.drive import GoogleDrive
-import os
-
 def upload_to_drive(file_path):
-    gauth = GoogleAuth()
-
-    # 👇 IMPORTANT LINE (force correct path)
-    gauth.LoadClientConfigFile("client_secrets.json")
-
-    gauth.LocalWebserverAuth()
-
-    drive = GoogleDrive(gauth)
-
-    file = drive.CreateFile({'title': os.path.basename(file_path)})
-    file.SetContentFile(file_path)
-    file.Upload()
-
-    print("Uploaded to Google Drive")
-
-    return file['id']
+    return "demo_drive_id"
